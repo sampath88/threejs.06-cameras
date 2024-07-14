64457f7bad9be27f5d6c60f848b44ef9b1e67225
sampath88 <sampathreddy8896@gmail.com>
2024-07-14 20:12:42 +0530

    look around 360

--- src/script.js
+++ src/script.js
@@ -73,8 +73,9 @@ let clock = new THREE.Clock();
 const tick = () => {
   // let elapsedTime = clock.getElapsedTime();
 
-  camera.position.x = cursor.x * 3;
-  camera.position.y = cursor.y * 3;
+  camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3;
+  camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3;
+  camera.position.y = cursor.y * 5;
   camera.lookAt(mesh.position);
   renderer.render(scene, camera);
   window.requestAnimationFrame(tick);
