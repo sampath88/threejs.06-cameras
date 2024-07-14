b9bbd702d56c614ed7e6152d1bac6cb36be213df
sampath88 <sampathreddy8896@gmail.com>
2024-07-14 20:33:42 +0530

    built in orbital controls

--- src/script.js
+++ src/script.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
 /* Cursor */
 const cursor = {
@@ -60,6 +61,10 @@ camera.position.z = 3;
 camera.lookAt(mesh.position);
 scene.add(camera);
 
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
 /**
  * Renderer
  */
@@ -73,10 +78,15 @@ let clock = new THREE.Clock();
 const tick = () => {
   // let elapsedTime = clock.getElapsedTime();
 
-  camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3;
-  camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3;
-  camera.position.y = cursor.y * 5;
-  camera.lookAt(mesh.position);
+  // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3;
+  // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3;
+  // camera.position.y = cursor.y * 5;
+  // camera.lookAt(mesh.position);
+
+  // Update controls
+  controls.update();
+
+  // Render
   renderer.render(scene, camera);
   window.requestAnimationFrame(tick);
 };
